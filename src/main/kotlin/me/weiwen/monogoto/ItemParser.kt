4f173e87377508c0e640d7856aca5199c084c022
Goh Wei Wen <goweiwen@gmail.com>
2021-02-03 22:07:23 +0800

    Add more item properties

--- src/main/kotlin/me/weiwen/monogoto/ItemParser.kt
+++ src/main/kotlin/me/weiwen/monogoto/ItemParser.kt
@@ -2,17 +2,25 @@ package me.weiwen.monogoto
 
 import com.uchuhimo.konf.Config
 import com.uchuhimo.konf.ConfigSpec
+import com.uchuhimo.konf.UnsetValueException
+import com.uchuhimo.konf.source.LoadException
 import com.uchuhimo.konf.source.Source
-import com.uchuhimo.konf.source.asSource
 import com.uchuhimo.konf.source.toml
 import com.uchuhimo.konf.source.yaml
+import com.uchuhimo.konf.toValue
 import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.enchantments.EnchantmentWrapper
 import java.io.File
 import java.util.logging.Level
 
 object Spec : ConfigSpec("") {
-    val material by required<Material>()
+    val material by required<String>()
     val name by optional<String?>(null)
+    val lore by optional<String?>(null)
+    val enchantments by optional<Map<String, Int>>(mapOf())
+    val unbreakable by optional<Boolean>(false)
+    val customModelData by optional<Int?>(null)
 }
 
 class ItemParser(private val plugin: Monogoto) {
@@ -26,15 +34,27 @@ class ItemParser(private val plugin: Monogoto) {
             else -> return null
         }
 
-        val config = Config { addSpec(Spec) }.withSource(source)
+        val config = try {
+            Config { addSpec(Spec) }.withSource(source)
+        } catch (e: LoadException) {
+            plugin.logger.log(Level.SEVERE, "${file.name}: ${e.message}")
+            return null
+        }
 
-        plugin.logger.log(Level.INFO, "$config")
-
-        config.validateRequired()
+        try {
+            config.validateRequired()
+        } catch (e: UnsetValueException) {
+            plugin.logger.log(Level.SEVERE, "${file.name}: ${e.message}")
+            return null
+        }
 
         return ItemTemplate(
-            config[Spec.material],
-            config[Spec.name]
+            config[Spec.material].let { Material.valueOf(it) },
+            config[Spec.name],
+            config[Spec.lore],
+            config[Spec.enchantments].mapKeys { (key, _) -> EnchantmentWrapper(key).enchantment },
+            config[Spec.unbreakable],
+            config[Spec.customModelData]
         )
     }
 }
--- src/main/kotlin/me/weiwen/monogoto/ItemTemplate.kt
+++ src/main/kotlin/me/weiwen/monogoto/ItemTemplate.kt
@@ -1,15 +1,27 @@
 package me.weiwen.monogoto
 
 import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 
-data class ItemTemplate(val material: Material, val name: String?) {
+data class ItemTemplate(
+    val material: Material,
+    val name: String?,
+    val lore: String?,
+    val enchantments: Map<Enchantment, Int>,
+    val unbreakable: Boolean,
+    val customModelData: Int?
+) {
     fun build(amount: Int = 1): ItemStack {
         return ItemStack(material, amount).apply {
             val itemMeta = this.itemMeta as ItemMeta
 
-            name?.let { itemMeta.setDisplayName(name) }
+            name?.let { itemMeta.setDisplayName(it) }
+            lore?.let { itemMeta.lore = it.lines() }
+            enchantments.forEach { (enchant, level) -> itemMeta.addEnchant(enchant, level, true) }
+            itemMeta.isUnbreakable = unbreakable
+            customModelData?.let { itemMeta.setCustomModelData(it) }
 
             this.itemMeta = itemMeta
         }
