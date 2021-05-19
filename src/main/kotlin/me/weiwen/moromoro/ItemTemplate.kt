161e97650d99a9dbbb16da60c89ddb7ff99dfbe7
Goh Wei Wen <goweiwen@gmail.com>
2021-05-19 15:15:46 +0800

    Add more item properties

--- src/main/kotlin/me/weiwen/moromoro/ItemTemplate.kt
+++ src/main/kotlin/me/weiwen/moromoro/ItemTemplate.kt
@@ -1,32 +1,56 @@
-@file:UseSerializers(MaterialSerializer::class, EnchantmentSerializer::class)
+@file:UseSerializers(MaterialSerializer::class, EnchantmentSerializer::class, UUIDSerializer::class)
 
 package me.weiwen.moromoro
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.UseSerializers
 import me.weiwen.moromoro.actions.Action
 import me.weiwen.moromoro.actions.Trigger
+import me.weiwen.moromoro.extensions.setHeadUrl
 import me.weiwen.moromoro.managers.ItemManager
 import me.weiwen.moromoro.serializers.EnchantmentSerializer
 import me.weiwen.moromoro.serializers.FormattedString
 import me.weiwen.moromoro.serializers.MaterialSerializer
+import me.weiwen.moromoro.serializers.UUIDSerializer
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
+import org.bukkit.attribute.Attribute
 import org.bukkit.enchantments.Enchantment
+import org.bukkit.inventory.EquipmentSlot
+import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 import org.bukkit.persistence.PersistentDataType
+import java.util.*
 
 @Serializable
 data class ItemProperties(
     val material: Material,
     val name: FormattedString? = null,
     val lore: FormattedString? = null,
+    @SerialName("custom-model-data")
     val customModelData: Int? = null,
+    val head: String? = null,
     val unbreakable: Boolean = false,
     val enchantments: Map<Enchantment, Int> = mapOf(),
+    val attributes: List<AttributeModifier> = listOf(),
+    val flags: List<ItemFlag> = listOf(),
     val triggers: Map<Trigger, List<Action>> = mapOf()
 )
+
+@Serializable
+data class AttributeModifier(
+    val attribute: Attribute,
+    val uuid: UUID,
+    val name: String,
+    val amount: Double,
+    val operation: org.bukkit.attribute.AttributeModifier.Operation,
+    val slot: EquipmentSlot,
+)
+
+val AttributeModifier.modifier: org.bukkit.attribute.AttributeModifier
+    get() = org.bukkit.attribute.AttributeModifier(uuid, name, amount, operation, slot)
 
 data class ItemTemplate(
     val key: String,
@@ -38,15 +62,23 @@ data class ItemTemplate(
 
             properties.name?.let { itemMeta.setDisplayName(it.value) }
             properties.lore?.let { itemMeta.lore = it.value.lines() }
+
             itemMeta.isUnbreakable = properties.unbreakable
+
             properties.customModelData?.let { data -> itemMeta.setCustomModelData(data) }
 
             properties.enchantments.forEach { (enchant, level) -> itemMeta.addEnchant(enchant, level, true) }
 
+            properties.attributes.forEach { itemMeta.addAttributeModifier(it.attribute, it.modifier) }
+
+            properties.flags.forEach { itemMeta.addItemFlags(it) }
+
             val data = itemMeta.persistentDataContainer
-            data.set(NamespacedKey(Moromoro.plugin.config.namespace, "key"), PersistentDataType.STRING, key)
+            data.set(NamespacedKey(Moromoro.plugin.config.namespace, "type"), PersistentDataType.STRING, key)
 
             item.itemMeta = itemMeta
+
+            properties.head?.let { item.setHeadUrl(properties.name?.value ?: "", it) }
         }
     }
 
--- src/main/kotlin/me/weiwen/moromoro/extensions/Item.kt
+++ src/main/kotlin/me/weiwen/moromoro/extensions/Item.kt
@@ -1,11 +1,32 @@
 package me.weiwen.moromoro.extensions
 
 import me.weiwen.moromoro.Moromoro
+import org.bukkit.Bukkit
 import org.bukkit.NamespacedKey
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
+import java.util.*
 
-val ItemStack.customItemKey: String? get() {
-    val data = itemMeta?.persistentDataContainer ?: return null
-    return data.get(NamespacedKey(Moromoro.plugin.config.namespace, "key"), PersistentDataType.STRING)
+val ItemStack.customItemKey: String?
+    get() {
+        val data = itemMeta?.persistentDataContainer ?: return null
+        return data.get(NamespacedKey(Moromoro.plugin.config.namespace, "type"), PersistentDataType.STRING)
+    }
+
+fun ItemStack.setHeadHash(name: String, hash: String) {
+    val url = "http://textures.minecraft.net/texture/$hash"
+    setHeadUrl(name, url)
+}
+
+fun ItemStack.setHeadUrl(name: String, url: String) {
+    val bytes = Base64.getEncoder().encode("{textures:{SKIN:{url:\"$url\"}}}".toByteArray())
+    setHeadBase64(name, String(bytes))
+}
+
+fun ItemStack.setHeadBase64(name: String, base64: String) {
+    val uuid = base64.hashCode()
+    Bukkit.getUnsafe().modifyItemStack(
+        this,
+        "{SkullOwner:{Name:\"$name\",Id:[I;-1,$uuid,-1,$uuid],Properties:{textures:[{Value:\"$base64\"}]}}}"
+    )
 }
