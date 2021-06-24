3356904502a388314894e2eb0262e8d90268abad
Goh Wei Wen <goweiwen@gmail.com>
2021-06-25 03:32:56 +0800

    Add sound string helper

--- src/main/kotlin/me/weiwen/moromoro/extensions/Sound.kt
+++ src/main/kotlin/me/weiwen/moromoro/extensions/Sound.kt
@@ -23,3 +23,19 @@ fun Block.playSoundAt(sound: Sound, category: SoundCategory, volume: Float, pitc
 fun Player.playSoundTo(sound: Sound, category: SoundCategory, volume: Float, pitch: Float) {
     this.playSound(location.clone().add(0.0, height/2, 0.0), sound, category, volume, pitch)
 }
+
+fun Location.playSoundAt(sound: String, category: SoundCategory, volume: Float, pitch: Float) {
+    world.playSound(this, sound, category, volume, pitch)
+}
+
+fun Entity.playSoundAt(sound: String, category: SoundCategory, volume: Float, pitch: Float) {
+    world.playSound(Location(world, location.x, location.y + height/2, location.z), sound, category, volume, pitch)
+}
+
+fun Block.playSoundAt(sound: String, category: SoundCategory, volume: Float, pitch: Float) {
+    world.playSound(Location(world, x + 0.5, y + 0.5, z + 0.5), sound, category, volume, pitch)
+}
+
+fun Player.playSoundTo(sound: String, category: SoundCategory, volume: Float, pitch: Float) {
+    this.playSound(location.clone().add(0.0, height/2, 0.0), sound, category, volume, pitch)
+}
