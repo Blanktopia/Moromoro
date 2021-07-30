d69b605146f079203780832d4e8b9e8135867d4b
Goh Wei Wen <goweiwen@gmail.com>
2021-07-31 01:24:54 +0800

    Add permissions actions

---
+++ src/main/kotlin/me/weiwen/moromoro/actions/RemovePermission.kt
@@ -0,0 +1,26 @@
+package me.weiwen.moromoro.actions
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import net.milkbowl.vault.permission.Permission
+import org.bukkit.Bukkit
+
+@Serializable
+@SerialName("remove-permission")
+data class RemovePermission(val command: String, val permissions: List<String>) : Action {
+    override fun perform(ctx: Context): Boolean {
+        val player = ctx.player ?: return false
+
+        if (!Bukkit.getServer().pluginManager.isPluginEnabled("Vault")) {
+            return false
+        }
+
+        val permissionsApi =
+            Bukkit.getServer().servicesManager.getRegistration(Permission::class.java)?.provider ?: return false
+        permissions.forEach {
+            permissionsApi.playerRemove(player, it)
+        }
+
+        return true
+    }
+}
