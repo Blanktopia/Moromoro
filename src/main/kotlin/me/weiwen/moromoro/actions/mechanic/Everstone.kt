962d2eb2197f2781d987998429c4b3cf9f1d675e
Goh Wei Wen <goweiwen@gmail.com>
2025-03-13 22:42:50 +0800

    Fix everstone on dolphins

--- src/main/kotlin/me/weiwen/moromoro/actions/mechanic/Everstone.kt
+++ src/main/kotlin/me/weiwen/moromoro/actions/mechanic/Everstone.kt
@@ -5,8 +5,8 @@ import kotlinx.serialization.Serializable
 import me.weiwen.moromoro.actions.Action
 import me.weiwen.moromoro.actions.Context
 import me.weiwen.moromoro.extensions.spawnParticle
-import org.bukkit.*
-import org.bukkit.entity.Breedable
+import org.bukkit.Particle
+import org.bukkit.entity.Ageable
 
 @Serializable
 @SerialName("everstone")
@@ -14,7 +14,7 @@ object Everstone : Action {
     override fun perform(ctx: Context): Boolean {
         val entity = ctx.entity ?: return false
 
-        if (entity is Breedable) {
+        if (entity is Ageable) {
             entity.ageLock = true
         }
         entity.spawnParticle(Particle.HEART, 2, 0.02)
